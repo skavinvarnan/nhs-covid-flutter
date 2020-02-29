ad9298ddf604234e3d3ea00921ee68c390c160fc
Kavin Varnan <skavinvarnan@gmail.com>
2020-02-29 11:26:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TestingMarkdown(),
+    );
+  }
+}
+
+class TestingMarkdown extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Text("asfd"),
+        ),
+      ),
+    );
+  }
+}
