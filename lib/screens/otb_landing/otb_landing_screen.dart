c8097a24f82ce9ab6290761ff80a64d79ca3b036
Kavin Varnan <skavinvarnan@gmail.com>
2020-02-29 12:39:31 +0530

    OTB landing

--- lib/screens/otb_landing/otb_landing_screen.dart
+++ lib/screens/otb_landing/otb_landing_screen.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
 
-class OrderTitleBodyLandingScreen extends StatelessWidget {
+import 'widgets/otb_landing_screen_widgets.dart';
+
+class OTBLandingScreen extends StatelessWidget {
+  final String title;
+  final String graphQlKey;
+
+  OTBLandingScreen({@required this.title, @required this.graphQlKey});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          this.title,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.all(8.0),
+          children: <Widget>[
+            OTBLandingButton(
+              title: "Guidelines",
+              bgColor: Colors.blue,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
